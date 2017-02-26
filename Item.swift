ea8132a0e538ca7cc87716c2acfe74b29e2cc2d1
cuongpc <cuongpc19@gmail.com>
2017-02-26 21:18:53 +0700

    first commit

---
+++ Item.swift
@@ -0,0 +1,21 @@
+//
+//  Item.swift
+//  TodoList
+//
+//  Created by AgribankCard on 2/26/17.
+//  Copyright © 2017 cuongpc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Item {
+    var name : String?
+    init(name : String?) {
+        self.name = name
+    }
+    init?() {
+        self.name = ""
+    }
+    
+    //1223
+}
