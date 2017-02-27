c3be25f46839925ff5145c916b486a69348cf373
cuongpc <cuongpc19@gmail.com>
2017-02-28 06:58:02 +0700

    Delete Item

--- TodoList/TodoListTableViewController.swift
+++ TodoList/TodoListTableViewController.swift
@@ -67,17 +67,29 @@ class TodoListTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            let currentItem = items[indexPath.row]
+            let indexPathrow = indexPath.row
+            
+            manageObjectContext.delete(currentItem)
+            do {
+                try manageObjectContext.save()
+            }
+            catch let error as NSError
+            {
+                print("error: \(error)")
+            }
+            //tableView.deleteRows(at: [indexPath], with: .fade)
+            fetchData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
